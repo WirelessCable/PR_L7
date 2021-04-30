777c0de5897100af2a55b88a77c4257809170be3
Rafał Bobka <pl.everrarder@o2.pl>
2021-04-30 14:02:09 +0200

    Lab 8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
+import { Route, Switch, Redirect } from "react-router-dom";
 import Posts from "./components/posts";
 import './App.css';
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
+import NotFound from "./components/notFound";
 
 function App() {
  return (
-   <div>
-        <Posts/>
-   </div>
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   component={Posts} />}
+                   />
+                   <Route path="/not-found" component={NotFound} />
+                   <Route path="/" exact component={Home} />
+                   <Redirect to="/not-found" />
+               </Switch>
+           </div>
+       </div>
+     </div>
  );
 }
 
